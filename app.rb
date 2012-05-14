a5dca5ed56550d7c8b6b221fe5174409a2316c81
Rogelio Zarate <i.am@rogeliozarate.com>
2012-05-13 21:01:34 -0500

    haml used to outline view

--- app.rb
+++ app.rb
@@ -8,13 +8,13 @@ require 'sass'
 
 set :haml, :format => :html5
 
-quotes = ["Actualiza tus datos en el IFE",
-          "Averigua quién es tu diputado y cómo contactarlo",
-          "Escribele a los candidatos sobre tus inquietudes",
-          "Lee la constitución",
-          "Conoce el reglamento de Policia y Transito de tu ciudad",
-          "Participa en los observatorio coudadanos",
-          "Integrate al comite de colonos de donde vives",
+quotes = ["Actualizar tus datos en el IFE",
+          "Averiguar quién es tu diputado y cómo contactarlo",
+          "Escribirle a los candidatos sobre tus inquietudes",
+          "Leer la Constitución",
+          "Conocer el reglamento de Policia y Transito de tu ciudad",
+          "Participar en los observatorio coudadanos",
+          "Integrarte al comite de colonos de donde vives",
           "VOTE AT YOUR LOCAL FUCKING ELECTIONS",
           "SHOW UP AT YOUR BORING ASS CITY HALL MEETINGS",
           "YOU SEE THAT FUCKING POTHOLE? REPORT THAT SHIT TO 311 ALREADY!",
@@ -24,6 +24,8 @@ quotes = ["Actualiza tus datos en el IFE",
           ]
 
 get '/' do
+  @quote =  quotes[Random.rand(quotes.size-1)]
+  @quote
   haml :index
 end
   
